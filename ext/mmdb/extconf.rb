6d945e951c1df95c5f21953f47edf04be4f44549
yoppi <y.hirokazu@gmail.com>
2017-02-10 11:52:52 +0900

    Add debug option

--- ext/mmdb/extconf.rb
+++ ext/mmdb/extconf.rb
@@ -1,5 +1,8 @@
 require 'mkmf'
 
 if have_header("maxminddb.h") && have_library("maxminddb")
+  if enable_config("debug")
+    CONFIG["debugflags"] << " -g3 -O0"
+  end
   create_makefile("mmdb")
 end
